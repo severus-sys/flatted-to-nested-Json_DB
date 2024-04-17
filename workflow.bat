bc532c5b9ee2d22ccde8b64bdc84675dc51e9112
Talat <108771246+severus-sys@users.noreply.github.com>
2024-04-17 17:07:53 +0300

    Update workflow.bat

--- workflow.bat
+++ workflow.bat
@@ -1,7 +1,8 @@
+
 @echo off
-py C:\Users\severus\Desktop\flatted-to-nested-Json_DB\get_json_and_fix.py
-py C:\Users\severus\Desktop\flatted-to-nested-Json_DB\get_data_json.py
-py C:\Users\severus\Desktop\flatted-to-nested-Json_DB\convert_flatten.py
-py C:\Users\severus\Desktop\flatted-to-nested-Json_DB\insert_data_to_pg.py
-echo Workflow completed. 
+py "%~dp0get_json_and_fix.py"
+py "%~dp0get_data_json.py"
+py "%~dp0convert_flatten.py"
+py "%~dp0insert_data_to_pg.py"
+echo Workflow completed.
 
